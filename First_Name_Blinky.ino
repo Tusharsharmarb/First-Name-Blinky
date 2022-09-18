0a1c65021cf2ff61b6c95f0bb25e8e8f6022510a
Tusharsharmarb <105912219+Tusharsharmarb@users.noreply.github.com>
2022-09-18 16:17:07 +0530

    Update First_Name_Blinky.ino

--- First_Name_Blinky.ino
+++ First_Name_Blinky.ino
@@ -1,31 +1,31 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);     // Initiallized the built-in LED of arduino nano and set it as OUTPUT
 }
 
-int unit_delay = 250;
+int unit_delay = 250;       // Assigned 250 value to unit_delay
 
-void dot()
+void dot()      // dot() function will represent the dot in morse code
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(unit_delay);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(unit_delay);
+  digitalWrite(LED_BUILTIN, HIGH);    // digitalwrite pin high will glow the LED
+  delay(unit_delay);                  // delay for 1 unit or 250 millisecond
+  digitalWrite(LED_BUILTIN, LOW);     // digitalwrite pin low will stop the LED
+  delay(unit_delay);                  // delay to get in between of every two representation
 }
-void dash()
+void dash()     // dash() function will represent the dot in morse code
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(unit_delay * 3);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(unit_delay);
+  digitalWrite(LED_BUILTIN, HIGH);     // digitalwrite pin high will glow the LED
+  delay(unit_delay * 3);                // delay for 3 unit or 750 millisecond
+  digitalWrite(LED_BUILTIN, LOW);       // digitalwrite pin low will stop the LED
+  delay(unit_delay);                  // delay to get in between of every two representation
 }
-void gap()
+void gap()    // gap() represent the gap represent that letter has beeen changed
 {
-  delay(unit_delay * 3);
+  delay(unit_delay * 3);        // // delay for 3 unit or 750 millisecond
 }
 
 
-void loop() {
+void loop() {               //this is my loop code which has my main code in which i have called dot(), dash() and gap() function respectively for the representation of the letter in morse code
   // put your main code here, to run repeatedly:
 
 
@@ -62,7 +62,5 @@ void loop() {
   dash();
   dot();
   gap();
-
-  exit(0);
 
 }
